92da87568df6a39a0277663cb2fdb24214342bc8
Theja <sugamanch@gmail.com>
2026-07-02 17:17:33 +0530

    Connected to aws s3 and pushed the all commits into cloud

--- backend/config/awe-config.js
+++ backend/config/awe-config.js
@@ -1,20 +1,16 @@
+require("dotenv").config();
+
 const AWS = require("aws-sdk");
 
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
-const region = process.env.AWS_REGION || "ap-south-1";
-
 AWS.config.update({
-  accessKeyId,
-  secretAccessKey,
-  region,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
 });
 
 const s3 = new AWS.S3();
 
-const S3_BUCKET = process.env.S3_BUCKET || "samplemyapnabucket";
-
 module.exports = {
   s3,
-  S3_BUCKET,
+  S3_BUCKET: process.env.S3_BUCKET,
 };
